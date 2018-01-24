bf807e18f518c90a424344c8b52623501dbe6458
Aidan Paetsch <aapaetsch@gmail.com>
2018-01-24 16:48:56 -0700

    xml layouts for both list and details, buttons to add sub and back to main work

--- app/src/main/java/com/example/paetsch_subbook/MainActivity.java
+++ app/src/main/java/com/example/paetsch_subbook/MainActivity.java
@@ -1,13 +1,34 @@
 package com.example.paetsch_subbook;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    String subs[] = new String [] {"netflix","spotify"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView list_view = (ListView) findViewById(R.id.list_view);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, subs);
+        list_view.setAdapter(adapter);
+        list_view.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+        });
+    }
+    public void newSub(View view){
+        Intent intent = new Intent(this,description.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/paetsch_subbook/description.java
@@ -0,0 +1,13 @@
+package com.example.paetsch_subbook;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class description extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_description);
+    }
+}
